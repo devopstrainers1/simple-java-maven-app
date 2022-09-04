9a0a1e5d2dbfdc542e629755376cf2d6f4b01e8d
devopstrainers1 <47707015+devopstrainers1@users.noreply.github.com>
2022-09-04 21:34:04 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,8 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.After
-import static org.junit.Assert.*
+import org.junit.After;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
