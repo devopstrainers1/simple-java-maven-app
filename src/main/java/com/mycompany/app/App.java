dd81ee7dbf1db928c6f886b5c7afa0ba5ca0dbd2
devopstrainers1 <47707015+devopstrainers1@users.noreply.github.com>
2022-10-15 12:52:15 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-        System.ou.println(new App().getMessage());
+        System.out.println(new App().getMessage());
     }
 
     private final String getMessage() {
